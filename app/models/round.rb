da3b99ffb06c6ea7a60675d8b8fe7623170a62f4
Wes <wesley.landriault@gmail.com>
2015-03-10 18:28:46 -0400

    Added model associations for companie, round and user model

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,2 +1,7 @@
 class Round < ActiveRecord::Base
+  belongs_to :company
+  has_many :due_diligences
+  has_many :investments
+  has_many :investors, through: :investments
+  belongs_to :lead_investor, class_name: 'User'
 end
