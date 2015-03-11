df7ff7fe3090d85d3532669b353ac6c0e3ee5dd3
jmy19 <jasonmyers@rogers.com>
2015-03-11 16:05:21 -0400

    created new company form and basic view

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,20 @@
 class CompaniesController < ApplicationController
   def index
+    @companies = Company.all
   end
 
   def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to companies_path
+    else
+      render :new, alert: 'Project could not be created.'
+    end
   end
 
   def new
+    @company = Company.new
+    
   end
 
   def show
@@ -21,6 +30,14 @@ class CompaniesController < ApplicationController
   end
 
   private
-  def user_params
+  def company_params
+    params.require(:company).permit(
+      :name,
+      :company_logo,
+      :elevator_pitch,
+      :location,
+      :industry_type,
+      :website_url
+    )
   end
 end
