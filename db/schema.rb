b981049f943cb087750307caf4fe0887ce3ef6fb
jonnylam <jonathanryanlam@gmail.com>
2015-03-17 17:34:36 -0400

    added follows model

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,19 @@ ActiveRecord::Schema.define(version: 20150317184415) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "follows", force: :cascade do |t|
+    t.integer  "followable_id",                   null: false
+    t.string   "followable_type",                 null: false
+    t.integer  "follower_id",                     null: false
+    t.string   "follower_type",                   null: false
+    t.boolean  "blocked",         default: false, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "follows", ["followable_id", "followable_type"], name: "fk_followables", using: :btree
+  add_index "follows", ["follower_id", "follower_type"], name: "fk_follows", using: :btree
+
   create_table "investments", force: :cascade do |t|
     t.integer  "investor_id"
     t.integer  "round_id"
