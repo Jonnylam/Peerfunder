19437935831d16029e1323d632b72fdc69597da6
Wes <wesley.landriault@gmail.com>
2015-03-20 03:02:19 -0400

    Made stylistic changes to nav bar.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,7 @@ class UserSessionsController < ApplicationController
 
   def create
     if @user = login(params[:email], params[:password], params[:remember])
-    redirect_back_or_to user_path(@user), notice: 'Login successful'
+    redirect_back_or_to users_dashboard_path, notice: 'Login successful'
      else
       flash.now[:alert] = 'Login failed'
       render action: 'new'
