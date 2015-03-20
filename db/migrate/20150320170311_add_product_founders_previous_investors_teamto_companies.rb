1deb65c1ff2c59a2152b6f92c5a2d010400391df
jonnylam <jonathanryanlam@gmail.com>
2015-03-20 13:28:16 -0400

    added some variables to companies

--- db/migrate/20150320170311_add_product_founders_previous_investors_teamto_companies.rb
+++ db/migrate/20150320170311_add_product_founders_previous_investors_teamto_companies.rb
@@ -1,4 +1,8 @@
 class AddProductFoundersPreviousInvestorsTeamtoCompanies < ActiveRecord::Migration
   def change
+  	add_column :companies, :product, :string 
+  	add_column :companies, :founders, :string
+  	add_column :companies, :previous_investors, :string
+  	add_column :companies, :team, :string 
   end
 end
