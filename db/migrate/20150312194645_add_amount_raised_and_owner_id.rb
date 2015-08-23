e352a7389410eb9907022ff45c023438d012d173
jonnylam <jonathanryanlam@gmail.com>
2015-08-22 22:30:26 -0400

    fixed amount_raised

--- db/migrate/20150312194645_add_amount_raised_and_owner_id.rb
+++
@@ -1,6 +0,0 @@
-class AddAmountRaisedAndOwnerId < ActiveRecord::Migration
-  def change
-    add_column :companies, :owner_id, :integer
-    add_column :rounds, :amount_raised, :integer, :default => 0
-  end
-end
